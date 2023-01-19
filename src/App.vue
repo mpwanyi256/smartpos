20ee19fadfc54e427c2dd1cc49b3a59d363d83a9
Mpwanyi Samuel <samuelmpwanyi@gmail.com>
2023-01-19 12:04:32 +0300

    Added vuetify3

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,27 @@
 <template>
   <div>
-    <a href="https://www.electronjs.org/" target="_blank">
-      <img src="./assets/electron.svg" class="logo electron" alt="Electron logo" />
-    </a>
-    <a href="https://vitejs.dev/" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Electron + Vite + Vue" />
-  <div class="flex-center">
-    Place static files into the <code>/public</code> folder
-    <img style="width:5em;" src="/node.svg" alt="Node logo">
+    <v-card
+    class="mx-auto"
+    width="400"
+    prepend-icon="mdi-home"
+  >
+    <template v-slot:title>
+      This is a title
+    </template>
+
+    <v-card-text>
+      This is content
+    </v-card-text>
+  </v-card>
   </div>
 </template>
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 <style>
-.flex-center {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo.electron:hover {
-  filter: drop-shadow(0 0 2em #9FEAF9);
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,33 @@ import "./style.css"
 import App from './App.vue'
 import './samples/node-api'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+// const app = createApp(App)
+// app.use(vuetify)
+// app.mount('#app').$nextTick(() => {
+//   postMessage({ payload: 'removeLoading' }, '*')
+// })
+
 createApp(App)
+  .use(vuetify)
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')
   })
+
+// new Vue({
+//   // router,
+//   // store,
+//   // vuetify,
+//   render: (h) => h(App),
+// }).$mount('#app');
